206901991fdea94ad38d53dd9c973099fd7b4a0d
seydanilsoyler <seydanilsoyler@gmail.com>
2026-01-13 23:50:43 +0300

    Comment out SQL queries in dvdrental_ödev9.sql

--- dvdrental_ödev9.sql
+++ dvdrental_ödev9.sql
@@ -5,5 +5,5 @@
 --INNER JOIN payment ON payment.customer_id = customer.customer_id;
 
 --3-SELECT rental_id, first_name, last_name FROM customer
-
 --INNER JOIN rental ON rental.customer_id = customer.customer_id;
+
